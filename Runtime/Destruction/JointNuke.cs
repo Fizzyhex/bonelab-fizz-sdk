c3c6d047c3bba64131c1e7b9b183aeef2b8f58a5
Fizzy <21183059+Fizzyhex@users.noreply.github.com>
2024-05-07 03:03:10 +0100

    Added JOINT NUKER

---
+++ Runtime/Destruction/JointNuke.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace FizzSDK.Destruction
+{
+    [AddComponentMenu("FizzSDK/Destruction Toolkit/Joint Nuke")]
+    public class JointNuke : MonoBehaviour
+    {
+        [SerializeField] private GameObject container;
+        
+        [ContextMenu("NUKE JOINTS!!!")]
+        public void NukeJoints()
+        {
+            foreach (var joint in container.GetComponentsInChildren<Joint>())
+            {
+                DestroyImmediate(joint);
+            }
+        }
+    }
+}
